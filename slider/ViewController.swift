6841593045a9d76d11519fb743939f11aa33048c
Shamil Kammaev <kammaev@mail.ru>
2020-03-02 10:28:15 +0300

    Initial Commit

---
+++ slider/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  slider
+//
+//  Created by Шамиль on 02.03.2020.
+//  Copyright © 2020 Shamil Kammaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorView: UIView!
+
+    
+    @IBOutlet weak var rSlider: UISlider!
+    @IBOutlet weak var gSlider: UISlider!
+    @IBOutlet weak var bSlider: UISlider!
+    
+   
+    @IBOutlet weak var redLabel: UILabel!
+    
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rSlider.tintColor = UIColor.red
+        gSlider.tintColor = UIColor.green
+        bSlider.tintColor = UIColor.blue
+        
+        
+        redLabel.text = ""
+        greenLabel.text = " "
+        blueLabel.text = " "
+        
+        
+        
+    }
+
+    func changeColor () {
+        colorView.backgroundColor = UIColor(red: CGFloat(rSlider.value), green: CGFloat(gSlider.value), blue: CGFloat(bSlider.value), alpha: 1)
+    }
+
+    
+@IBAction func rgbSliderChanged(_ sender: Any) {
+      changeColor()
+       redLabel.text = String(rSlider.value)
+       greenLabel.text = String(gSlider.value)
+       blueLabel.text = String(bSlider.value)
+    
+    }
+    
+
+    
+ 
+        
+    
+    //redLabel.text = String(rSlider.value)
+    //greenLabel.text = String(gSlider.value)
+    //blueLabel.text = String(bSlider.value)
+    
+    
+}
+
